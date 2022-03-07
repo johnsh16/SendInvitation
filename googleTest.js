f15b55edc594494abda98b5bbe8a88058036be03
johnsh16 <hunterjohns118@gmail.com>
2022-03-07 10:02:51 -0500

    Commit
    Calendar test is successful

--- googleTest.js
+++ googleTest.js
@@ -11,9 +11,9 @@ fs.readFile('credentials.json', (err, content) => {
 })
 
 function authorize(credentials, callback) {
-    const {private_key, client_id, auth_uri} = credentials;
+    const {client_secret, client_id, redirect_uris} = credentials.installed;
     const oAuth2Client = new google.auth.OAuth2(
-        client_id, private_key, auth_uri
+        client_id, client_secret, redirect_uris[0]
     )
 
     fs.readFile(TOKEN_PATH, (err, token) => {
@@ -25,28 +25,28 @@ function authorize(credentials, callback) {
 
 function getAccessToken(oAuth2Client, callback) {
     const authUrl = oAuth2Client.generateAuthUrl({
-        access_type: 'offline',
-        scope: SCOPES
-    })
-    console.log('Authorize this app by visiting this url:', authUrl)
+      access_type: 'offline',
+      scope: SCOPES,
+    });
+    console.log('Authorize this app by visiting this url:', authUrl);
     const rl = readline.createInterface({
-        input: process.stdin,
-        output: process.stdout
-    })
+      input: process.stdin,
+      output: process.stdout,
+    });
     rl.question('Enter the code from that page here: ', (code) => {
-        rl.close();
-        oAuth2Client.getToken(code, (err, token) => {
-            if (err) return console.error('Error retrieving access token', err)
-            oAuth2Client.setCredentials(token)
-
-            fs.writeFile(TOKEN_PATH, JSON.stringify(token, (err) => {
-                if (err) return console.error(err)
-                console.log('Token stored to', TOKEN_PATH)
-            }))
-            callback(oAuth2Client)
-        })
-    })
-}
+      rl.close();
+      oAuth2Client.getToken(code, (err, token) => {
+        if (err) return console.error('Error retrieving access token', err);
+        oAuth2Client.setCredentials(token);
+        // Store the token to disk for later program executions
+        fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
+          if (err) return console.error(err);
+          console.log('Token stored to', TOKEN_PATH);
+        });
+        callback(oAuth2Client);
+      });
+    });
+  }
 
 function listEvents(auth) {
     const calendar = google.calendar({version: 'v3', auth})
